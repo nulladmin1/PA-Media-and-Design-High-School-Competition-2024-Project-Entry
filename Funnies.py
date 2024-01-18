5ee5f6b49920386ecb969e550c56ee3086d2d0a6
nulladmin1 <140198242+nulladmin1@users.noreply.github.com>
2024-01-18 11:05:22 -0500

    Edited comments

--- Funnies.py
+++ Funnies.py
@@ -1,18 +1,16 @@
-try:
-    import flet as ft # Don't forget to pip
-except ImportError:
-    raise ImportError('Flet not installed. ')
+try: # Checks if Flet is installed
+    import flet as ft # Imports the flet module, a framework for creating programs using Python in the flutter SDK
+except ImportError: # If flet isn't installed, it raises an ImportError that it's isn't installed and gives instructions how to install
+    raise ImportError('Flet not installed. Please run:\npip -r requirements.txt\n(Recommended to use venv or conda.)')
 
-def main(page): #kinda ovious 
-    def add_clicked(e): #button comand
-        page.add(ft.Checkbox(label=new_task.value)) #adds a checkbox gui
+def main(page): # Creates a main function; needed for flet
+    def add_clicked(e): # Button comand
+        page.add(ft.Checkbox(label=new_task.value)) # Adds a checkbox in the GUI
         new_task.value = "" # Clears the text in the new_task text field
-        new_task.focus() #This means that after clicking the "Add" button, the cursor focus is moved to the new_task
-        new_task.update() #upate page
+        new_task.focus() # This means that after clicking the "Add" button, the cursor focus is moved to the new_task
+        new_task.update() # Updates the page
 
     new_task = ft.TextField(hint_text="Whats needs to be done?", width=300) #basicly prints on the screen
     page.add(ft.Row([new_task, ft.ElevatedButton("Add", on_click=add_clicked)])) #button command
 
-ft.app(target=main)# setting as app
-
-
+ft.app(target=main)# Sets the main function as a Flet app
