bf5d4e59d69effea2486475388e63e773fb14e95
nulladmin1 <shrey.deogade@protonmail.com>
2024-01-26 11:24:42 -0500

    Updated comment for ImportError

--- Funnies.py
+++ Funnies.py
@@ -1,12 +1,12 @@
 try: # Checks if Kivy is installed
-    from kivy.app import App # Imports the Kivy module, a cross-platform GUI-framework for creating programs using Python
+    from kivy.app import App # Imports from the Kivy module, a cross-platform GUI-framework for creating programs using Python
     from kivy.uix.widget import Widget
 except ImportError: # If Kivy isn't installed, it raises an ImportError that it's isn't installed and gives instructions how to install
-    raise ImportError('Kivy not installed. Please run:\npip -r requirements.txt\n(Recommended to use venv or conda.)')
+    raise ImportError('Kivy not installed. Please read the README.md to properly run the program. ')
 
 
-class RecommenderApp(App):
+class RecommenderApp(App): # Instantaniates Class RecommenderApp for a Kivy App
     pass
 
-if __name__ == "__main__":
+if __name__ == "__main__": # Checks if file is being run, if it is, then it runs the App, or if it's being used as a module, doesn't run
     RecommenderApp().run()
