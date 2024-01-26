36d1b9e5d778263fc720ae3a63b53048b7eed4e2
nulladmin1 <shrey.deogade@protonmail.com>
2024-01-25 21:01:57 -0500

    Change to using Kivy instead of Flet

--- Funnies.py
+++ Funnies.py
@@ -1,16 +1,12 @@
-try: # Checks if Flet is installed
-    import flet as ft # Imports the flet module, a framework for creating programs using Python in the flutter SDK
-except ImportError: # If flet isn't installed, it raises an ImportError that it's isn't installed and gives instructions how to install
-    raise ImportError('Flet not installed. Please run:\npip -r requirements.txt\n(Recommended to use venv or conda.)')
+try: # Checks if Kivy is installed
+    from kivy.app import App # Imports the Kivy module, a cross-platform GUI-framework for creating programs using Python
+    from kivy.uix.widget import Widget
+except ImportError: # If Kivy isn't installed, it raises an ImportError that it's isn't installed and gives instructions how to install
+    raise ImportError('Kivy not installed. Please run:\npip -r requirements.txt\n(Recommended to use venv or conda.)')
 
-def main(page): # Creates a main function; needed for flet
-    def add_clicked(e): # Button comand
-        page.add(ft.Checkbox(label=new_task.value)) # Adds a checkbox in the GUI
-        new_task.value = "" # Clears the text in the new_task text field
-        new_task.focus() # This means that after clicking the "Add" button, the cursor focus is moved to the new_task
-        new_task.update() # Updates the page
 
-    new_task = ft.TextField(hint_text="Whats needs to be done?", width=300) #basicly prints on the screen
-    page.add(ft.Row([new_task, ft.ElevatedButton("Add", on_click=add_clicked)])) #button command
+class RecommenderApp(App):
+    pass
 
-ft.app(target=main)# Sets the main function as a Flet app
+if __name__ == "__main__":
+    RecommenderApp().run()
