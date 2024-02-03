c9667961bdbac505559e9ea98ec8700f67477c96
nulladmin1 <shrey.deogade@protonmail.com>
2024-02-02 21:53:16 -0500

    Updated nix-shell for new packages

--- shell.nix
+++ shell.nix
@@ -2,11 +2,26 @@
 
 let
   pythonPkgs = ps: with ps; [
-    cv2
-    deepface
     kivy
     pandas
     requests
+    opencv4
+
+    # DeepFace
+    (
+      buildPythonPackage rec {
+        pname = "deepface";
+        version = "0.0.83";
+        src = fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-yxpES7KTIVP++3gBb1V7gGmbCFf5uFfNmSgbg2I03O4=";
+        };
+        doCheck = false;
+        propagatedBuildInputs = with pkgs; [
+          python311Packages.gunicorn
+        ];
+      }
+    )
   ];
   python = pkgs.python3.withPackages pythonPkgs;
 in python.env
